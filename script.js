9799c93c227702d2048caeafd87bbe7f6ac5862a
Alex Godin <mistergodin@gmail.com>
2022-01-15 23:49:40 -0700

    updating hw

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+alert("PaSsWoRd GeNeRaToR");
+confirm("ALRIGHTY THEN...Okay or cancel?")
+prompt("choose number between 8-128")
+confirm("Lowercase Allowed")
+confirm("Uppercase Allowed")
+confirm("Numbers Allowed")
+confirm("Symbols Allowed")
 
-  passwordText.value = password;
+const password = document.querySelector('#password')
+const button = document.querySelector('#button')
 
+let characters = ('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890`~!@#$%^&*()-_=+[{]};:,<.>/?');
+let passwordlength = 128;
+let passwordValue= '';
+
+
+
+const createPassword = () => {
+  passwordValue = '';
+} 
+generateBtn.addEventListener('click', () => {
+  const length = +lengthEL.value;
+  const hasLower = lower
+
+for (let i = 0; i < passwordLength; i++) {
+  let number = Math.floor(Math.random() * characters.length);
+  passwordValue += characters.substring(number+ 1);
+
+  password.value = passwordValue;
 }
+  console.log(length)
+})
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+button.addEventListener('click', createPassword)
+// Write password to the #password input
+
+
